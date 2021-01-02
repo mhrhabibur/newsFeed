0fab11b6585dbcf607fe121082ac1190c0e4e282
Habibur Rahman <mhr.habibur@gmail.com>
2021-01-02 19:49:47 +0600

    action bar added

--- app/src/main/java/com/example/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.newsfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton backButton, refreshButton;
+
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backButton = findViewById(R.id.backButton);
+        refreshButton = findViewById(R.id.refreshkButton);
+
+        recyclerView = findViewById(R.id.recyclerViewID);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "BackButton is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Refresh Button is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        //getSupportActionBar().hide();
     }
 }
